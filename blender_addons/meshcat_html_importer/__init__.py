8f3687f99db96a4b242f18a2b85a3f76fe588de0
nepfaff <nepfaff@mit.edu>
2026-05-12 11:56:05 -0400

    Bump meshcat importer version to 0.1.3

--- blender_addons/meshcat_html_importer/__init__.py
+++ blender_addons/meshcat_html_importer/__init__.py
@@ -12,7 +12,7 @@ from . import operators
 bl_info = {
     "name": "Meshcat HTML Importer",
     "author": "Nicholas Pfaff",
-    "version": (0, 1, 2),
+    "version": (0, 1, 3),
     "blender": (5, 0, 0),  # Blender 5.0+ only
     "location": "File > Import > Meshcat Recording (.html, .zip, .meshcat)",
     "description": "Import meshcat recordings with geometry and animation",
